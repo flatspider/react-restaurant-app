1f0962f5627edcec31fe4b19141ecfa874234ce2
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-03-04 16:27:14 -0500

    Cleaned up code. Corrected class references.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import MenuForm from "./components/MenuForm";
 import CheckOut from "./components/CheckOut";
 
 function App() {
-  // Establish what initial state is.
-  // Should be lunch.
   const [checkOutItems, setCheckOutItems] = useState("");
-
   const [menuSelection, setMenuSelection] = useState("a");
 
   const placeCheckOutItemInCart = ({ items }) => {
@@ -20,23 +17,20 @@ function App() {
 
   const removeItemFromCart = ({ items }) => {
     alert(items.description); // This returns a unique ID string.
-    // Where should the check out live? The state should be passed to CheckOut.
-    const checkOutCopy1 = [...checkOutItems]; // This should be adding to an array.
-    // What does the button know? It can view the object it is attached to.
-    // I want the button to splice out the array index it is currently at.
+    const checkOutCopy1 = [...checkOutItems];
+
     let deleteIndex = checkOutCopy1.findIndex(
       (item) => item.description === items.description
     );
-    console.log(deleteIndex);
+
     checkOutCopy1.splice(deleteIndex, 1);
     setCheckOutItems(checkOutCopy1); // Not updating the state value.
-    console.log(checkOutItems); // ID values are being added to state. I want the entire item added.
   };
 
   return (
     <div className="App container mt-5">
       <header className="App-header">
-        <div class="top-banner">
+        <div className="top-banner">
           <img src="https://static.wixstatic.com/media/a895db_d25ab21086c64ec89c658622a8248f2e~mv2.jpg/v1/fill/w_402,h_478,al_c,lg_1,q_80,enc_auto/brickstreetcafelogo.jpg" />
           <h1>
             Kitchen is open for lunch Tuesday - Saturday from 11am - 4pm
